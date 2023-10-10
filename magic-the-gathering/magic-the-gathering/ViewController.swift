baac0f64fb4bbe74bc34d36e98e24cc2838e6ca4
Maxime Nicolas <71704827+Maxime-UwU@users.noreply.github.com>
2023-10-10 16:35:03 +0200

    initialise project

---
+++ magic-the-gathering/magic-the-gathering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic-the-gathering
+//
+//  Created by Maxime NICOLAS on 10/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
