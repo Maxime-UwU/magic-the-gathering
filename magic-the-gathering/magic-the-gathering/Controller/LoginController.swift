21b168d0aa565a8f403c096c91548331d8201c9a
Vahé Krikorian <vahekrikorian@MacBook-Pro.local>
2023-10-11 14:10:42 +0200

    add login and register

---
+++ magic-the-gathering/magic-the-gathering/Controller/LoginController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  magic-the-gathering
+//
+//  Created by Maxime NICOLAS on 10/10/2023.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+       
+    @IBAction func goRegister(_ sender: Any) {
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "Register") as? RegisterViewController {
+            // Masquer la barre d'onglets lorsque SecondViewController est poussé
+            vc.hidesBottomBarWhenPushed = true
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
