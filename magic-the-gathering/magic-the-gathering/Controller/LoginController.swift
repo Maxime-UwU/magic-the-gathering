5897a422ca08786978debb09b2f2e90519cb1efa
Vahé Krikorian <vahekrikorian@MacBook-Pro.local>
2023-10-11 17:03:31 +0200

    update

--- magic-the-gathering/magic-the-gathering/Controller/LoginController.swift
+++ magic-the-gathering/magic-the-gathering/Controller/LoginController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class LoginController: UIViewController {
-       
+    
     @IBAction func goRegister(_ sender: Any) {
         if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "Register") as? RegisterViewController {
             // Masquer la barre d'onglets lorsque SecondViewController est poussé
@@ -18,10 +18,38 @@ class LoginController: UIViewController {
         }
     }
     
+    var allUsers: [[String: AnyObject]] = []
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func buttonLogin(_ sender: UIButton) {
+//        for user in allUsers {
+//            
+//        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let users = jsonResult["users"] as? [[String: AnyObject]] {
+                    allUsers = users
+                    for user in users {
+                        print(user["username"]!)
+                        print(user["password"]!)
+                        print(allUsers)
+                    }
+                }
+            } catch {
+                // handle error
+            }
+        }
     }
 }
 
